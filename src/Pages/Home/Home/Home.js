1493437ba2295ad5b80610a06f1f0826ed965027
Neamul01 <neamulhaque2002@gmail.com>
2022-05-10 20:14:02 +0600

    theme added , tailwind,daisy added and pics added

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <button class="btn btn-primary">Button</button>
+        </div>
+    );
+};
+
+export default Home;
