0f7383e941d10a9169d3866ba3434bbdb2540220
Neamul01 <neamulhaque2002@gmail.com>
2022-05-15 12:52:50 +0600

    login page added

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            Login
+        <div className='flex h-screen justify-center items-center'>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <div className="card-body">
+                    <h2 className="card-title justify-center">Login</h2>
+                    <div className='mt-5'>
+                        <div className="form-control w-full">
+                            <lable className="label-text text-left">Email</lable>
+                            <input type="email" placeholder="Email..." className="input input-bordered w-full max-w-xs" />
+                        </div>
+                        <div className="form-control w-full max-w-xs mt-3">
+                            <lable className="label-text text-left">Password</lable>
+                            <input type="password" placeholder="Password..." className="input input-bordered w-full max-w-xs" />
+                        </div>
+                        <label className="label mb-2">
+                            <span className="label-text-alt">Forgot Password?</span>
+                        </label>
+                        <button className="btn w-full mb-2">Login</button>
+                        <span className="label-text-alt text-sm">New to Doctors Portal? <Link className='text-secondary' to={'/signup'}>Create New Account</Link></span>
+
+                    </div>
+                    <div className="divider">OR</div>
+                    <div className='w-full'>
+                        <button className="btn w-full btn-outline">Continue With Google</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
