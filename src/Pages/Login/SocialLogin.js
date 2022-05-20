0423d8d654073b2ee7dc1c759c627e6b48d8d842
Neamul01 <neamulhaque2002@gmail.com>
2022-05-20 22:53:47 +0600

    method problem solved

--- src/Pages/Login/SocialLogin.js
+++ src/Pages/Login/SocialLogin.js
@@ -2,10 +2,12 @@ import React, { useEffect } from 'react';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init'
+import useToken from '../../hooks/useToken';
 import LoadingSpinner from '../Shared/LoadingSpinner';
 
 const SocialLogin = () => {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const [token] = useToken(user);
 
     const location = useLocation();
     const navigate = useNavigate();
@@ -17,10 +19,10 @@ const SocialLogin = () => {
     let signinError;
 
     useEffect(() => {
-        if (user) {
+        if (token) {
             navigate(from, { replace: true });
         }
-    }, [user, from, navigate])
+    }, [token, from, navigate])
 
     if (loading) {
         return <LoadingSpinner></LoadingSpinner>
