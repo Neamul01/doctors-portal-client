b80f0f270d3fbb1a2a71ff38776ed9cbee3a8ea0
Neamul01 <neamulhaque2002@gmail.com>
2022-05-22 03:29:16 +0600

    client secret take from server

--- src/Pages/Dashboard/CheckoutForm.js
+++ src/Pages/Dashboard/CheckoutForm.js
@@ -1,9 +1,30 @@
-import React from 'react';
-import { CardElement, Elements, useElements, useStripe } from '@stripe/react-stripe-js';
+import React,{useState, useEffect} from 'react';
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 
-const CheckoutForm = () => {
+const CheckoutForm = ({appointment}) => {
     const stripe = useStripe();
     const elements = useElements();
+    const [cardError, setCardError]= useState('');
+    const [clientSecret, setClientSecret]=useState('');
+
+    const {price}=appointment;
+
+    useEffect(()=>{
+fetch('http://localhost:5000/create-payment-intent',{
+    method:'POST',
+        headers:{
+            'content-type':'application/json',
+            'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+        },
+        body: JSON.stringify({price})
+})
+.then(res=>res.json())
+.then(data=>{
+    if(data?.clientSecret){
+        setClientSecret(data.clientSecret)
+    }
+})
+    },[price])
 
     const handleSubmit = async (event) => {
         event.preventDefault()
@@ -16,8 +37,17 @@ const CheckoutForm = () => {
         if (card === null) {
             return
         }
+
+        const {error, paymentMethod} = await stripe.createPaymentMethod({
+            type: 'card',
+            card,
+        });
+
+        setCardError(error?.message||'')
+          
     }
     return (
+       <>
         <form onSubmit={handleSubmit}>
             <CardElement
                 options={{
@@ -35,10 +65,14 @@ const CheckoutForm = () => {
                     },
                 }}
             />
-            <button type="submit" className=' btn btn-md btn-success mt-4' disabled={!stripe}>
+            <button type="submit" className=' btn btn-md btn-success mt-4' disabled={!stripe||!clientSecret}>
                 Pay
             </button>
         </form>
+        {
+            cardError && <p className='text-red-500'>{cardError}</p>
+        }
+       </>
     );
 };
 
--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -39,7 +39,7 @@ const Payment = () => {
                 <div className="card flex-shrink-0 w-full max-w-lg shadow-2xl bg-base-100">
                     <div className="card-body">
                         <Elements stripe={stripePromise}>
-                            <CheckoutForm />
+                            <CheckoutForm appointment={appointment}/>
                         </Elements>
 
                     </div>
